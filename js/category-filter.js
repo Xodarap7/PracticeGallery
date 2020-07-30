e5557451f087223ec6d7be21433325de47f01697
Xodarap7 <basyar86@gmail.com>
2020-07-30 14:09:53 +0500

    filter's good

--- js/category-filter.js
+++ js/category-filter.js
@@ -1,20 +1,34 @@
 const categoriesMenu = document.querySelectorAll("#category-menu");
 const images = document.querySelectorAll("#photo");
 const imagesArray = Array.from(images);
+const galeryContainer = document.querySelector(".galery-container");
 
 categoriesMenu.forEach((categoryMenu) => {
   categoryMenu.addEventListener("click", () => {
+    addAll();
     const filter = categoryMenu.getAttribute("class");
+    deleteAll();
     filterImages(filter);
   });
 });
 
-galeryContainer = document.querySelector(".galery-container");
 function filterImages(filter) {
   const filteredImages = imagesArray.filter((image) =>
     image.classList.contains(filter)
   );
-  console.log(filteredImages);
+  filteredImages.forEach((filteredImage) => {
+    galeryContainer.appendChild(filteredImage);
+  });
+}
 
-  galeryContainer.appendChild(filteredImages);
+function addAll() {
+  images.forEach((image) => {
+    galeryContainer.appendChild(image);
+  });
+}
+
+function deleteAll() {
+  images.forEach((image) => {
+    galeryContainer.removeChild(image);
+  });
 }
