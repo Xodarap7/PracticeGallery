185a414afdf2ad5ef319317db428ebac9ea9646b
Xodarap7 <basyar86@gmail.com>
2020-07-30 16:34:32 +0500

    bug fixed in gallery js

--- js/category-filter.js
+++ js/category-filter.js
@@ -1,7 +1,12 @@
+// Фильтр картинок
+
 const categoriesMenu = document.querySelectorAll("#category-menu");
-const images = document.querySelectorAll("#photo");
+let images = document.querySelectorAll("#photo");
 const imagesArray = Array.from(images);
 const galeryContainer = document.querySelector(".galery-container");
+let filteredImages;
+
+document.addEventListener("load", () => {});
 
 categoriesMenu.forEach((categoryMenu) => {
   categoryMenu.addEventListener("click", () => {
@@ -13,11 +18,12 @@ categoriesMenu.forEach((categoryMenu) => {
 });
 
 function filterImages(filter) {
-  const filteredImages = imagesArray.filter((image) =>
+  filteredImages = imagesArray.filter((image) =>
     image.classList.contains(filter)
   );
   filteredImages.forEach((filteredImage) => {
     galeryContainer.appendChild(filteredImage);
+    images = filteredImages;
   });
 }
 
@@ -31,4 +37,75 @@ function deleteAll() {
   images.forEach((image) => {
     galeryContainer.removeChild(image);
   });
+}
+
+// модальное окно
+
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modal img");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-window");
+const nextPrevious = document.querySelector(".next-previous");
+const previous = document.querySelector(".previous");
+const next = document.querySelector(".next");
+
+function closePhoto() {
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  closeBtn.style.display = "none";
+  nextPrevious.style.display = "none";
+}
+
+function openPhoto() {
+  modal.style.display = "block";
+  closeBtn.style.display = "block";
+  overlay.style.display = "block";
+  nextPrevious.style.display = "block";
+}
+
+let route;
+images.forEach((photo) => {
+  photo.addEventListener("click", () => {
+    openPhoto();
+    route = photo.getAttribute("src");
+    modalImg.setAttribute("src", route);
+    closeBtn.addEventListener("click", () => {
+      closePhoto();
+    });
+    overlay.addEventListener("click", () => {
+      closePhoto();
+    });
+  });
+});
+next.addEventListener("click", nextImage);
+previous.addEventListener("click", previousImage);
+
+function previousImage() {
+  for (let i = 0; i < images.length; i++) {
+    if (modalImg.src === images[i].src) {
+      nextImageNew = images[i].previousElementSibling;
+    }
+  }
+
+  if (nextImageNew === null) {
+    modalImg.src = images[images.length - 1].src;
+  } else {
+    modalImg.src = nextImageNew.src;
+  }
+}
+
+let nextImageNew;
+function nextImage() {
+  for (let i = 0; i < images.length; i++) {
+    if (modalImg.src === images[i].src) {
+      nextImageNew = images[i].nextElementSibling;
+      console.log(images.length);
+    }
+  }
+
+  if (nextImageNew === null) {
+    modalImg.src = images[0].src;
+  } else {
+    modalImg.src = nextImageNew.src;
+  }
 }
